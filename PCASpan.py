1f411dd3965fc8a96137b4c0b516c488cbf310cb
Cameron <cameron.farzaneh@gmail.com>
2018-01-12 15:24:48 -0500

    Autoencoder done

--- PCASpan.py
+++ PCASpan.py
@@ -5,11 +5,12 @@ from scipy import linalg
 from VAE_Models.VAE import VAE as model
 from VAE_Models.architectures import DNN
 import tensorflow as tf
+from tqdm import tqdm
 
-def genData():
-    dataset = np.ndarray((1000,3))
+def genData(size):
+    dataset = np.ndarray((size,3))
 
-    for i in range(1000):
+    for i in range(size):
         theRange = 5
         x = np.random.uniform(-theRange, theRange)
         y = np.random.uniform(-theRange, theRange)
@@ -20,7 +21,7 @@ def genData():
 
     return dataset
 
-dataset = genData()
+dataset = genData(1000)
 
 dataTrans = dataset.transpose()
 cov = np.matmul(dataTrans, dataset)
@@ -45,7 +46,6 @@ X, Y, Z, U, V, W = zip(*soa)
 ax.quiver(X, Y, Z, U, V, W, color='r')
 
 ax.scatter(dataset[:,0], dataset[:,1], dataset[:,2])
-plt.show()
 
 print("\nRunning Linear Autoencoder with two dimensional latency space...\n")
 
@@ -54,7 +54,7 @@ latency_dim = 2
 theEncoder = [] # num neurons in each layer of encoder network
 theDecoder = [] # num neurons in each layer of generator network
 
-batch_size=100
+batch_size=1000
 learning_rate=0.001
 
 encoder = DNN(theEncoder)
@@ -68,3 +68,22 @@ hyperParams = {'reconstruct_cost': 'gaussian',
 			  }
 
 autoEncoder = model(input_dim, encoder, latency_dim, decoder, hyperParams)
+
+for i in tqdm(range(10000)):
+	cost, reconstr_loss, KL_loss = autoEncoder(genData(batch_size))
+
+weights = autoEncoder.get_latent_weights()[0]
+
+print(weights)
+print(weights[:,0])
+
+a1 = np.concatenate((arr,weights[:,0]*5))
+a2 = np.concatenate((arr,weights[:,1]*5))
+
+soaa = np.array([a1,a2])
+
+X, Y, Z, U, V, W = zip(*soaa)
+
+ax.quiver(X, Y, Z, U, V, W, color='g')
+
+plt.show()
