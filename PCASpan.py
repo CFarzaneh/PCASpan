207953086f986dcb3ef3ccfa24bd9298bbf5b7c6
Cameron <cameron.farzaneh@gmail.com>
2018-01-19 17:35:11 -0500

    Rewritten in PyTorch :)

--- PCASpan.py
+++ PCASpan.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from scipy import linalg
-from VAE_Models.VAE import VAE as model
-from VAE_Models.architectures import DNN
-import tensorflow as tf
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
 from tqdm import tqdm
 import sys
 
@@ -50,6 +50,58 @@ ax.scatter(dataset[:,0], dataset[:,1], dataset[:,2])
 
 print("\nRunning Linear Autoencoder with two dimensional latency space...\n")
 
+Batch_Size = 1000
+LearningRate = 0.002
+
+class AutoEncoder(nn.Module):
+    def __init__(self):
+        super(AutoEncoder, self).__init__()
+
+        self.encoder = nn.Sequential(
+            nn.Linear(3, 2),
+        )
+        self.decoder = nn.Sequential(
+            nn.Linear(2, 3),
+        )
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return encoded, decoded
+
+autoencoder = AutoEncoder()
+
+optimizer = torch.optim.Adam(autoencoder.parameters(), lr=LearningRate)
+loss_func = nn.MSELoss()
+
+for _ in tqdm(range(50)):
+	for i in tqdm(range(10000)):
+		x = torch.Tensor(genData(Batch_Size))
+		b_x = Variable(x.view(-1, 3))
+		b_y = Variable(x.view(-1, 3))
+
+		encoded, decoded = autoencoder(b_x)
+
+		loss = loss_func(decoded, b_y)
+		optimizer.zero_grad()
+		loss.backward()
+		optimizer.step()
+
+	weights = autoencoder.encoder[0].weight.data.numpy()
+
+	a1 = np.concatenate((arr,weights[0]*5))
+	a2 = np.concatenate((arr,weights[1]*5))
+
+	soaa = np.array([a1,a2])
+
+	X, Y, Z, U, V, W = zip(*soaa)
+
+	ax.quiver(X, Y, Z, U, V, W, color='g')
+
+plt.show()
+
+
+'''
 input_dim = (3,1)
 latency_dim = 2
 theEncoder = [] # num neurons in each layer of encoder network
@@ -75,14 +127,14 @@ for j in tqdm(range(50)):
                 data = genData(batch_size)
                 #print("Data = ", data)
                 cost, reconstr_loss, KL_loss = autoEncoder(data)
-                '''
+
                 print("output = ", autoEncoder.reconstruct(data))
                 print("cost = ", cost)
                 print("alpha = ", autoEncoder.alpha)
                 print("reconstruction loss = ", reconstr_loss)
                 print("KL_Loss = ", KL_loss)
                 sys.exit()
-                '''
+
         weights = autoEncoder.get_latent_weights()[0]
         weights = weights.transpose()
         print(weights)
@@ -100,3 +152,4 @@ for j in tqdm(range(50)):
         autoEncoder.reset()
 
 plt.show()
+'''
